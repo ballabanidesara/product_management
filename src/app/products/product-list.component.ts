836140852dfcb2c1aa859ef3612d0b9707ca8268
ballabanidesara <desara.ballabani@base-com.al>
2022-04-28 15:59:02 +0200

    Handling Events with Event Binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -10,6 +10,7 @@ export class ProductListComponent {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number =2;
+    showImage: boolean = false;
     products: any[] = [
          {
                 "productId": 2,
@@ -33,5 +34,8 @@ export class ProductListComponent {
               },
         
     ]
+    toggleImage(): void {
+      this.showImage = !this.showImage
+    }
 
 }
