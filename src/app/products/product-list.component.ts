f506ad32647dee712880e8e8b459eb5fe2feb0c2
ballabanidesara <desara.ballabani@base-com.al>
2022-04-29 11:43:36 +0200

    Getters and Setters

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -8,12 +8,23 @@ import { IProduct } from "./product";
 
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number =2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    private _listFilter = '';
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this.performFilter(value);
+    }
+  
+
+    filteredProducts: IProduct[] = [];
     products: IProduct[] = [
          {
                 "productId": 2,
@@ -34,11 +45,20 @@ export class ProductListComponent {
                 "price": 8.9,
                 "starRating": 4.8,
                 "imageUrl": "assets/images/hammer.png"
-              },
+              }
         
-    ]
+    ];
+
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().includes(filterBy));
+    }
     toggleImage(): void {
       this.showImage = !this.showImage
+    }
+    ngOnInit(): void {
+      this._listFilter = 'cart';
     }
 
 }
