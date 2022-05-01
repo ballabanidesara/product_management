e461c1eb7d4029f4af0928477eacd80ba9995ca2
ballabanidesara <desara.ballabani@base-com.al>
2022-05-02 00:33:29 +0200

    Retrieving Data using HTTP

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -9,11 +10,13 @@ import { ProductService } from "./product.service";
 
 })
 
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number =2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
 
     private _listFilter = '';
     get listFilter(): string {
@@ -38,8 +41,17 @@ export class ProductListComponent implements OnInit {
       this.showImage = !this.showImage
     }
     ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products,
+          this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+      });
+      
+    }
+    ngOnDestroy(): void {
+      this.sub.unsubscribe();
     }
     onRatingClicked(message: string): void {
       this.pageTitle = 'Product Listt: '+ message;
